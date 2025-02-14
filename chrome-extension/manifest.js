317fe771e7ff850d31786cb89bf74da0592a8a1c
Bobby Lee <superbahbi@gmail.com>
2025-02-13 19:52:04 -0800

    Remove new tab content ui and devtools

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -50,9 +50,6 @@ const manifest = withSidePanel({
     default_popup: "popup/index.html",
     default_icon: "icon-34.png",
   },
-  chrome_url_overrides: {
-    newtab: "new-tab/index.html",
-  },
   icons: {
     128: "icon-128.png",
   },
@@ -63,14 +60,9 @@ const manifest = withSidePanel({
     },
     {
       matches: ["http://*/*", "https://*/*", "<all_urls>"],
-      js: ["content-ui/index.iife.js"],
-    },
-    {
-      matches: ["http://*/*", "https://*/*", "<all_urls>"],
       css: ["content.css"], // public folder
     },
   ],
-  devtools_page: "devtools/index.html",
   web_accessible_resources: [
     {
       resources: ["*.js", "*.css", "*.svg", "icon-128.png", "icon-34.png"],
